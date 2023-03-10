68e51dad86bafd16b33f034c078ba0f4dff30e39
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-10 06:16:35 +0300

    yıldızlarla piramit ve farklı çeşitlerde üçgenler oluşturuldu

---
+++ FibonacciTriangleandAvarage/Fibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciTriangleandAvarage
+{
+    public class Fibonacci
+    {
+        public int _count { get; set; }
+        public int[] f;
+        public double avarage;
+        public Fibonacci(int count, int n)
+        {
+            _count = count;
+            this.f = new int[n];
+        }
+        public void fib(int[] f, int count)
+        {
+
+            f[1] = 1;
+            f[2] = 1;
+
+            for (int i = 3; i <= count; i++)
+            {
+
+
+                f[i] = f[i - 1] + f[i - 2];
+            }
+        }
+        public void avarageFunction()
+        {
+            avarage = f.Average();
+            Console.Write("Fibonacci dizi ortalaması:{0}    ", avarage);
+        }
+    }
+}
+
--- FibonacciTriangleandAvarage/Program.cs
+++ FibonacciTriangleandAvarage/Program.cs
@@ -1,41 +1,36 @@
-﻿using PatikadevCSharpProjeleri;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FibonacciTriangle
+namespace FibonacciTriangleandAvarage
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-          
             Console.WriteLine("Fibonacci serisinin yüksekliğini giriniz:");
-            int  height= Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
             fiboTriangle(height);
-            
-
         }
-        
         public static void fiboTriangle(int n)
         {
-            
+
             int length = n * (n + 1) / 2; //fibonacci üçgen yüksekliğinden serinin eleman sayısını bulduk
-        
-            Fibonacci fibonacci = new Fibonacci(length,length+1);
+
+            Fibonacci fibonacci = new Fibonacci(length, length + 1);
 
 
             fibonacci.fib(fibonacci.f, length);
 
-            
+
             int fiboNum = 1;
 
-           
+
             for (int i = 1; i <= n; i++)
             {
-               
+
                 for (int j = 1; j <= i; j++)
                     Console.Write(fibonacci.f[fiboNum++] + " ");
 
@@ -43,7 +38,7 @@ namespace FibonacciTriangle
             }
 
             fibonacci.avarageFunction();
-            
+
         }
     }
 }
