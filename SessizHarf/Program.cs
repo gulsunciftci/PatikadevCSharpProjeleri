bde5b83be0b87048386154aeedad12d1a39c87c5
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-12 02:51:48 +0300

    Sessiz Harf programı yazıldı

--- SessizHarf/Program.cs
+++ SessizHarf/Program.cs
@@ -12,54 +12,35 @@ namespace SessizHarf
         {
             var strings = Console.ReadLine().Split(' ');
             string unluler = "aeoöuüıi";
-            int index=0;
-            int unsuzSayisi=0;
             string sonuc = "";
-            
-            for(int i=0; i < strings.Length; i++)
+            bool[] trueFalse = new bool[strings.Length];
+            int sıra = 0; 
+            foreach (var item in strings) 
             {
-                string b = "";
-                unsuzSayisi = 0;
-       
-                for (int j = 0; j < strings[i].Length; j++)
+                int counter = 0; 
+                foreach (var harf in item)
                 {
-                    if (unluler.Contains(strings[i][j]))
-                    {
-                        if (unsuzSayisi == 1)
-                        {
-                            unsuzSayisi = 0;
-                        }
-                        continue;
-                    }
-                    if (unsuzSayisi!=2)
-                    {
-                        unsuzSayisi++;
-               
-                        if (unsuzSayisi == 1)
-                        {
-                            index = j;             
-                        }
 
-                        else if (unsuzSayisi == 2)
+                    if (unluler.Contains(harf)) 
+                    {
+                        counter = 0; 
+                    }
+                    else
+                    {
+                        counter++; 
+                        if (counter == 2) 
                         {
-                            if ((j - index) == 1)
-                            {
-                                 b+= "true" + " ";    
-                            }
-                            else
-                            {
-                                index = j;
-                            }
+                            trueFalse[sıra] = true;
+                            break; 
                         }
                     }
-                  
                 }
-                if (b == "")
-                {
-                     b+= "false" + " ";
-                }
-                
-                sonuc += b;
+                sıra++; 
+
+            }
+            foreach (var item in trueFalse)
+            {
+                sonuc+=item + " ";
             }
             Console.WriteLine(sonuc);
         }
