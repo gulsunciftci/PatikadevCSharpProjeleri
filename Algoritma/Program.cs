60d794110edf6cfc6cb93ff790b5ef22c0bf70ef
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-03-11 14:58:06 +0300

    Kullanıcıdan aralarında virgül olan bir string ve rakam alıp stringden o rakam indexindeki harfi silen bir program yazdım

---
+++ Algoritma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Remove remove = new Remove(Console.ReadLine());
+            remove.removeFunction();
+            
+
+        }
+        
+    }
+}
---
+++ Algoritma/Remove.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma
+{
+    public class Remove
+    {
+        public int index { get; set; }
+        public string str { get; set; }
+        public string newStr { get; set; }
+        
+        public Remove(string str)
+        {
+            this.str = str;  
+        }
+
+        public void removeFunction()
+        {
+            var strings = str.Split(',');
+            index = Convert.ToInt32(strings[1]);
+            newStr = strings[0];
+            newStr = newStr.Remove(index, 1);
+            Console.WriteLine(newStr);
+        }
+        
+    }
+}
